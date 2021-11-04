56b7b60ba27d541cddff6a8d98bda9df0a0ee510
henriqueleite42 <henriqueleite616@gmail.com>
2021-11-04 13:46:55 +0000

    Deploying to release from @ techmmunity/discord-bot-v3@1b65f363fdf7fb93a547f3538c18fe756d9ef82e 🚀

--- events/someone-can-help-me/index.js
+++ events/someone-can-help-me/index.js
@@ -2,11 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.someoneCanHelpMe = void 0;
 const qce_1 = require("../../interactions/qce");
-const someoneCanHelpMe = async (message) => {
-    const content = message.content.toLowerCase();
+const isSomeone = (content) => {
     const hasSomeone = content.includes("alguém") || content.includes("alguem");
     const hasHelp = content.includes("ajuda");
-    if (hasSomeone && hasHelp) {
+    const hasUnderstands = content.includes("manja de");
+    return hasSomeone && (hasHelp || hasUnderstands);
+};
+const isHow = (content) => {
+    const hasHow = content.includes("como");
+    const hasI = content.includes("eu");
+    const hasSolve = content.includes("resolvo");
+    return hasHow && hasI && hasSolve;
+};
+const someoneCanHelpMe = async (message) => {
+    const content = message.content.toLowerCase();
+    if (isSomeone(content) || isHow(content)) {
         await message.reply({
             embeds: [qce_1.QCE_EMBED],
         });
