1b65f363fdf7fb93a547f3538c18fe756d9ef82e
Henrique Leite <henriqueleite616@gmail.com>
2021-11-04 10:46:11 -0300

    Add More QCE options

--- src/events/someone-can-help-me/index.ts
+++ src/events/someone-can-help-me/index.ts
@@ -1,13 +1,29 @@
 import { Message } from "discord.js";
 import { QCE_EMBED } from "../../interactions/qce";
 
+const isSomeone = (content: string) => {
+	const hasSomeone = content.includes("alguém") || content.includes("alguem");
+	const hasHelp = content.includes("ajuda");
+	const hasUnderstands = content.includes("manja de");
+
+	// Alguém ajuda
+	// Alguém manja de
+	return hasSomeone && (hasHelp || hasUnderstands);
+};
+
+const isHow = (content: string) => {
+	const hasHow = content.includes("como");
+	const hasI = content.includes("eu");
+	const hasSolve = content.includes("resolvo");
+
+	// Como eu resolvo
+	return hasHow && hasI && hasSolve;
+};
+
 export const someoneCanHelpMe = async (message: Message) => {
 	const content = message.content.toLowerCase();
 
-	const hasSomeone = content.includes("alguém") || content.includes("alguem");
-	const hasHelp = content.includes("ajuda");
-
-	if (hasSomeone && hasHelp) {
+	if (isSomeone(content) || isHow(content)) {
 		await message.reply({
 			embeds: [QCE_EMBED],
 		});
