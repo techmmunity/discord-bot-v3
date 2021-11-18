7acdbfd7c1f3ba0c9a0f9558d4d49652b72a247c
Henrique Leite <henriqueleite616@gmail.com>
2021-11-18 20:28:09 -0300

    Fix Clean Command

--- src/interactions/clean/index.ts
+++ src/interactions/clean/index.ts
@@ -1,7 +1,6 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { isBetween } from "@techmmunity/utils";
 import { CommandInteraction, TextChannel } from "discord.js";
-import { COLORS } from "../../assets/colors";
 import { RAZAL_ID } from "../../config/ids";
 import { Interaction } from "../../types/interactions";
 import { PermissionTypeEnum } from "../../enums/permission-type";
@@ -15,21 +14,12 @@ export const clean = async (interaction: CommandInteraction) => {
 
 	const messages = await channel.messages.fetch({
 		// eslint-disable-next-line @typescript-eslint/no-magic-numbers
-		limit: isBetween(qtd, 0, 100) ? qtd : 100,
+		limit: isBetween(qtd, 1, 100) ? qtd : 100,
 	});
 
 	const messagesToDelete = messages.filter(message => !message.pinned);
 
 	await channel.bulkDelete(messagesToDelete);
-
-	await interaction.editReply({
-		embeds: [
-			{
-				title: "Done!",
-				color: COLORS.green,
-			},
-		],
-	});
 };
 
 export const cleanCommand: Interaction = {
