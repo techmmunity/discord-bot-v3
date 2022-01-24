5cf142b0e5572d009b9272a52e5371beb92bcb0d
henriqueleite42 <henriqueleite616@gmail.com>
2022-01-24 22:52:47 +0000

    Deploying to release from @ techmmunity/discord-bot-v3@118e3db4fee1366f92b4d5b27a63db6458b43300 🚀

--- events/send-welcome-msg/index.js
+++ events/send-welcome-msg/index.js
@@ -4,7 +4,6 @@ exports.sendWelcomeMsg = void 0;
 const ids_1 = require("../../config/ids");
 const get_channel_1 = require("../../utils/get-channel");
 const make_welcome_image_1 = require("../../interactions/welcome-img/make-welcome-image");
-const qce_1 = require("../../interactions/qce");
 const sendWelcomeMsg = async (member) => {
     if (member.guild.id !== ids_1.TECHMMUNITY_GUILD_ID)
         return;
@@ -13,9 +12,6 @@ const sendWelcomeMsg = async (member) => {
     await generalChannel.send({
         content: `<@${member.user.id}>`,
         files: [welcomeImg],
-        embeds: [
-            Object.assign(Object.assign({}, qce_1.QCE_EMBED), { title: "Aqui está nosso tutorial sobre como fazer perguntas:" }),
-        ],
     });
 };
 exports.sendWelcomeMsg = sendWelcomeMsg;
