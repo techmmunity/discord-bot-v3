118e3db4fee1366f92b4d5b27a63db6458b43300
Henrique Leite <henriqueleite616@gmail.com>
2022-01-24 19:51:55 -0300

    Remove Welcome Msg

--- src/events/send-welcome-msg/index.ts
+++ src/events/send-welcome-msg/index.ts
@@ -4,7 +4,6 @@ import { GuildMember } from "discord.js";
 import { GENERAL_CHANNEL_ID, TECHMMUNITY_GUILD_ID } from "../../config/ids";
 import { getTextChannel } from "../../utils/get-channel";
 import { makeWelcomeImg } from "../../interactions/welcome-img/make-welcome-image";
-import { QCE_EMBED } from "../../interactions/qce";
 
 export const sendWelcomeMsg = async (member: GuildMember) => {
 	if (member.guild.id !== TECHMMUNITY_GUILD_ID) return;
@@ -16,11 +15,5 @@ export const sendWelcomeMsg = async (member: GuildMember) => {
 	await generalChannel.send({
 		content: `<@${member.user.id}>`,
 		files: [welcomeImg],
-		embeds: [
-			{
-				...QCE_EMBED,
-				title: "Aqui está nosso tutorial sobre como fazer perguntas:",
-			},
-		],
 	});
 };
