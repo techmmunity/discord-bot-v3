a073821590a6d5fdc952f9d10a919cdcbac09407
henriqueleite42 <henriqueleite616@gmail.com>
2022-02-12 14:24:56 +0000

    Deploying to release from @ techmmunity/discord-bot-v3@c1db442d5c59744aa01bee4e7d716e0e3bec1654 🚀

--- interactions/qce/index.js
+++ interactions/qce/index.js
@@ -8,10 +8,12 @@ exports.QCE_EMBED = {
     title: "Hey, parece que você fez uma pergunta incompleta :tired_face:",
     description: [
         "Aqui está o nosso tutorialzinho sobre dicas para fazer perguntas:",
-        '- **Evite perguntar se "alguém pode te ajudar"**, não perca tempo com isso, envie direto a **sua pergunta**, com um **print do seu código** e um **print do erro** (caso exista)',
+        '- **Faça perguntas diretas**, não perca tempo perguntando se "alguém esta dispponivel", se "alguem pode te ajudar", envie direto a **sua pergunta**, descrevendo **com detalhes** o que você quer fazer, com um **print do seu código** e um **print do erro** (caso exista)',
+        '- **Envie suas perguntas nos canais corretos**, ali na esquerda <<<< temos um tópico "❓ TOPICS", onde se encontram canais de diversas linguagens e assuntos. Sempre poste suas perguntas naqueles canais.',
         '- **Não peça respostas prontas**, "estou com esse problema aqui, como eu resolvo?". Os membros de nossa comunidade preferem te ajudar a **entender o seu problema**, para que você consiga resolvê-lo sozinho no futuro :wink:',
         '- **Evite fazer perguntas muito amplas**, como "Como eu faço um site?", prefira perguntas menores sobre problemas específicos, como "O que eu começo a aprender para poder fazer um site?".',
         "- **Não envie DMs para os membros**, a maior parte da galera não gosta de receber DMs e não vai te responder, pergunte diretamente nos canais.",
+        "Pra reforçar: **NÃO ENVIE DMS PARA OS MEMBROS**, ninguém gosta de ser incomodado nas DMs, ok? :wink:",
         "Agora que você já conhece nossas dicas, está pronto para fazer suas perguntas! Boa sorte e fique a vontade para postar quantas perguntas quiser, ficaremos felizes em ajudar! :smile:",
     ].join("\n\n"),
     color: colors_1.COLORS.turquoise,
